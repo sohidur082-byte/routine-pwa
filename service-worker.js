365ea5eec007b9735baed6999a2a1e66cab25612
sohidur082-byte <sohidur082@gmail.com>
2026-01-19 03:49:36 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "routine-pwa-v1";
+
+const FILES_TO_CACHE = [
+  "/routine-pwa/",
+  "/routine-pwa/index.html",
+  "/routine-pwa/manifest.json",
+  "/routine-pwa/icon-192.png",
+  "/routine-pwa/icon-512.png"
+];
+
+// Install
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
